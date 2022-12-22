a7696f9ac43842d1cd3cc0238d3620f74923132b
MSURASH <sufianrash22@gmail.com>
2022-12-22 03:00:14 -0500

    First Commit

---
+++ src/main/java/com/java/FromGithubJenkinsSpringBootAppToDockerApplication.java
@@ -0,0 +1,13 @@
+package com.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FromGithubJenkinsSpringBootAppToDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FromGithubJenkinsSpringBootAppToDockerApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/java/controllers/MyController.java
@@ -0,0 +1,15 @@
+package com.java.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+
+	@GetMapping("/jd")
+	public String print() {
+		
+
+		return "SPRING BOOT APPLICATION THROUGH JENKINS TO DOCKER...";
+	}
+}
